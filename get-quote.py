2ac9b16dd9f2ffff7b9d6a54947d09f7025a2800
eacruz3 <79220631+eacruz3@users.noreply.github.com>
2021-02-17 16:01:56 -0600

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
+import random
 def principal():
   print("Mantenlo Lógicamente Impresionante.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last=13
+  rnd=random.randint(0.last)
+  print(rnd)
 
 if __name__== "__main__":
   principal()
